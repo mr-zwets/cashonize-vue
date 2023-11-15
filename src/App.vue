c0e008ed82dfaf52c8f6f8afcbe13b926651e5bc
Mathieu Geukens <mr-zwets@protonmail.com>
2023-11-15 13:27:10 +0100

    add balances onMount, import indexeddb-storage

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
-  import { Wallet } from "mainnet-js"
+  import { TestNetWallet, BaseWallet,BalanceResponse  } from "mainnet-js"
+  import IndexedDBProvider from "@mainnet-cash/indexeddb-storage"
   import HelloWorld from './components/HelloWorld.vue'
 
   // reactive state
-  const wallet = ref(null as (Wallet | null)) 
+  const wallet = ref(null as (TestNetWallet | null)) 
+  const balance = ref(0 as (number | undefined));
+  const nrTokenCategories = ref(0 as (number | undefined));
 
   // functions that mutate state and trigger updates
   onMounted(async()=>{
-    wallet.value = await Wallet.newRandom() as Wallet;
+    //BaseWallet.StorageProvider = IndexedDBProvider ;
+    wallet.value = await TestNetWallet.fromSeed("", "m/44'/0'/0'/0/0") as TestNetWallet;
+    const walletBalance = await wallet.value.getBalance() as BalanceResponse;
+    const getFungibleTokensResponse = await wallet.value.getAllTokenBalances();
+    const getNFTsResponse = await wallet.value.getAllNftTokenBalances();
+    let tokenCategories = Object.keys({...getFungibleTokensResponse, ...getNFTsResponse})
+    // update state
+    balance.value = walletBalance.sat;
+    nrTokenCategories.value = tokenCategories.length;
   })
 </script>
 
 <template>
-  <header>
-    
-  </header>
-
   <main>
     <div class="wrapper">
-      <HelloWorld msg="Vue + Typescript + mainnet-js" />
+      <HelloWorld msg="Cashonize-Vue" />
     </div>
     <br>
+    <div>
+      BCH balance:  
+      <span style="color: hsla(160, 100%, 37%, 1);">
+        {{ balance ? balance + " sats" : "" }}
+      </span>
+    </div>
+    <div>
+      Wallet token balance: 
+      <span style="color: hsla(160, 100%, 37%, 1);">
+        {{ nrTokenCategories ? nrTokenCategories + " different token categories" : ""}}
+      </span>
+    </div>
     <div> {{ wallet?.address ?? "" }} </div>
-    <br>
     <div> {{ wallet?.tokenaddr ?? "" }} </div>
   </main>
 </template>
