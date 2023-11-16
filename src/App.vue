f3ca45527be8016014afda72e36d1c5fdd7df1b5
Mathieu Geukens <mr-zwets@protonmail.com>
2023-11-16 16:22:32 +0100

    add send-bch fieldset, add navbar & add chota

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
   import bchWalletView from "./components/bchWallet.vue"
+  import myTokensView from "./components/myTokens.vue"
   import HelloWorld from './components/HelloWorld.vue'
   import { ref, onMounted } from 'vue'
-  import { TestNetWallet, BaseWallet, BalanceResponse } from "mainnet-js"
+  import { Wallet, TestNetWallet, BaseWallet, BalanceResponse } from "mainnet-js"
+  import { IndexedDBProvider } from "@mainnet-cash/indexeddb-storage"
 
   // reactive state
   const wallet = ref(null as (TestNetWallet | null));
+  const network = ref("chipnet" as ("mainnet" | "chipnet"));
   const balance = ref(0 as (number | undefined));
   const nrTokenCategories = ref(0 as (number | undefined));
+  const displayView = ref(1);
+
+  function changeView(newView: number){
+    displayView.value = newView;
+  }
 
   onMounted(async()=>{
-    //BaseWallet.StorageProvider = IndexedDBProvider ;
-    wallet.value = await TestNetWallet.fromSeed("", "m/44'/0'/0'/0/0") as TestNetWallet;
+    // @ts-ignore
+    BaseWallet.StorageProvider = IndexedDBProvider;
+    const walletClass = (network.value == "mainnet") ? Wallet: TestNetWallet;
+    const seedphrase = "";
+    wallet.value = await walletClass.fromSeed(seedphrase, "m/44'/0'/0'/0/0") as TestNetWallet;
     const walletBalance = await wallet.value.getBalance() as BalanceResponse;
     const getFungibleTokensResponse = await wallet.value.getAllTokenBalances();
     const getNFTsResponse = await wallet.value.getAllNftTokenBalances();
@@ -23,19 +34,28 @@
 </script>
 
 <template>
-  <main>
+  <main style="margin-top: 20px; max-width: 75rem; margin: auto;">
     <div class="wrapper">
       <HelloWorld msg="Cashonize-Vue" />
     </div>
     <nav style="display: flex; margin: 20px 0px;">
-      <div style="padding: 0px 10px;">BchWallet</div>
-      <div style="padding: 0px 10px;">MyTokens</div>
+      <div @click="changeView(1)">BchWallet</div>
+      <div @click="changeView(2)">MyTokens</div>
+      <div @click="changeView(3)">
+        <img style="vertical-align: text-bottom;" src="images/settings.svg">
+      </div>
     </nav>
-    <bchWalletView :wallet="(wallet as TestNetWallet)" :balance="balance" :nrTokenCategories="nrTokenCategories"/>
+    <bchWalletView v-if="displayView == 1" :wallet="(wallet as TestNetWallet | null )" :balance="balance" :nrTokenCategories="nrTokenCategories" :network="network"/>
+    <myTokensView v-if="displayView == 2" />
   </main>
 </template>
 
 <style scoped>
+nav div{
+  padding: 1rem 1.8rem;
+  cursor: pointer;
+  border-bottom: 2px solid var(--color-lightGrey);
+}
 header {
   line-height: 1.5;
 }
--- src/components/bchWallet.vue
+++ src/components/bchWallet.vue
@@ -3,24 +3,52 @@
   import { TestNetWallet  } from "mainnet-js"
   import { defineCustomElements } from '@bitjson/qr-code';
 
-  const { wallet, balance, nrTokenCategories } = defineProps<{
+  const { wallet, balance, nrTokenCategories, network } = defineProps<{
     wallet: TestNetWallet | null,
     balance?: number,
-    nrTokenCategories?: number
+    nrTokenCategories?: number,
+    network: ("mainnet" | "chipnet");
   }>()
 
   defineCustomElements(window);
 
   // reactive state
   const displayeBchQr = ref(true);
+  const sendAmount = ref(0);
+  const sendAddr = ref("");
+
+  const explorerUrlMainnet = "https://explorer.bitcoinunlimited.info";
+  const explorerUrlChipnet = "https://chipnet.chaingraph.cash";
+  let explorerUrl = (network == "mainnet")? explorerUrlMainnet : explorerUrlChipnet;
 
   function switchAddressTypeQr(){
     displayeBchQr.value = !displayeBchQr.value;
+  }
+  async function maxSendAmount(){
+    try{
+      if(!wallet) return;
+      const maxAmountToSend = await wallet.getMaxAmountToSend();
+      if(!maxAmountToSend.sat) throw("expected a number")
+      sendAmount.value = maxAmountToSend.sat;
+    } catch(error) {
+      console.log(error)
+    }
+  }
+  async function sendBch(){
+    try{
+      if(!wallet) return;
+      const { txId } = await wallet.send([{ cashaddr: sendAddr.value, value: sendAmount.value, unit: "sat" }]);
+      alert(`Sent ${sendAmount.value} sats to ${sendAddr.value} \n${explorerUrl}/tx/${txId}`);
+      console.log(`Sent ${sendAmount.value} sats to ${sendAddr.value} \n${explorerUrl}/tx/${txId}`);
+    } catch(error){
+      console.log(error)
+    }
   }
 </script>
 
 
 <template>
+  <fieldset style="margin-top: 20px; padding-top: 2rem; max-width: 75rem; margin: auto;">
     <div>
       BCH balance:  
       <span style="color: hsla(160, 100%, 37%, 1);">
@@ -41,4 +69,21 @@
     <div style="text-align: center;">
       <div id="switchAddress" class="icon" @click="switchAddressTypeQr()" style="font-size: 20px;font-weight: 700;width: fit-content; margin: auto; margin-top: -10px; cursor: pointer;">⇄</div>
     </div>
+  </fieldset>
+  <fieldset style="margin-top: 15px;">
+    <legend>Send BCH to</legend>
+    <div class="inputGroup">
+      <span class="addressInput">
+        <input v-model="sendAddr" id="sendAddr" placeholder="address">
+      </span>
+      <span class="sendAmountGroup">
+        <span style="width: 100%;" class="input-icon input-icon-right">
+          <input v-model="sendAmount" id="sendAmount" type="text" placeholder="amount">
+          <i id="sendUnit" style="color: black;">BCH</i>
+        </span>
+        <button @click="maxSendAmount()" style="color: black;">max</button>
+      </span>
+    </div>
+    <input @click="sendBch()" type="button" class="primaryButton" id="send" value="Send">
+  </fieldset>
 </template>
